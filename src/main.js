b9126d56782baf26c66b49e1501201031b03f0f5
Jackie <1433118948@qq.com>
2017-08-29 09:42:14 +0800

    设置侧边memu
    第一次修改

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+
+import './common/sass/index.sass';
+
+
+Vue.use(iView);
+
+Vue.config.productionTip = false;
+
+fastclick.attach(document.body);
+/* eslint-disable no-new */
+
+new Vue({
+  el: '#app',
+  router: router,
+  render: h => h(App)
+});
